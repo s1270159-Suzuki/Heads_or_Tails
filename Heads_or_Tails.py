79e5ac5e42eb21c98881f926a4056fe863ca9ed2
Haruya Suzuki <nuanyelingmu@gmail.com>
2020-08-04 16:53:58 +0900

    HoT: Initial revision

---
+++ Heads_or_Tails.py
@@ -0,0 +1,26 @@
+import random
+
+def main():
+  k = 0
+  l = 0
+  print('Tssing a coin...')
+
+  for i, a in enumerate(range(3)):
+    b = Heads_or_Tails()
+    print('Round {}: {}'.format(i+1,b))
+    if(b == 'Heads'):
+      k = k + 1
+    else:
+      l = l + 1
+
+  print('Heads: {}, Tails: {}'.format(k,l))
+    
+
+def Heads_or_Tails():
+  if(random.randrange(2) == 0):
+    return 'Heads'
+  else:
+    return 'Tails'
+
+if __name__ == '__main__':
+  main()
